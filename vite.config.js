dc0ab30b75aef52686abe759bef6aa8a16f38ff3
codecounter <ImCodeCounter@gmail.com>
2023-11-06 23:38:15 +0800

    new file:   .github/workflows/deploy.yml 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vitePluginRequire from "vite-plugin-require";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginRequire.default()],
+  base: "/CodeCounter-Portfolio/"
 })
 
 
